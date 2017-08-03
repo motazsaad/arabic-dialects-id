2055ab5c44caaf134fbc148e1c5e9a7d09c3ad1f
Motaz Saad <motaz.saad@gmail.com>
2017-08-03 15:47:30 +0300

    push a first prototype

--- test.py
+++ test.py
@@ -3,9 +3,10 @@ arabic_dialect_model = open('multidialect_model/model').read()
 identifier = LanguageIdentifier.from_modelstring(arabic_dialect_model, norm_probs=True)
 
 print(identifier.classify("كيفك شو اخبارك انا منيح"))
-
+# ('jo', 0.9931649962526201)
 print(identifier.classify("انت عملت كده ليه"))
-
+# ('eg', 0.9984723495959299)
 print(identifier.classify("من شان شو"))
-
+# ('pa', 0.6983566048467883)
 print(identifier.classify("صعيبة برشة باهي"))
+# ('tn', 0.9997509389390998)
