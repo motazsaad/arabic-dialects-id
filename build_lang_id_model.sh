61006ac7df42f4947c79396ae5f08be7a785f42a
Motaz Saad <motaz.saad@gmail.com>
2017-08-24 23:44:50 +0300

    add steps file

--- build_lang_id_model.sh
+++ build_lang_id_model.sh
@@ -2,33 +2,47 @@
 
 
 if [ $# -ne 3 ]; then
-    echo "usage ${0} python_cmd n train_corpus";
+    echo "usage ${0} python_cmd train_corpus n";
     exit -1;
 fi
 
 
 python_cmd=${1}
-n=${2}
-train_corpus=${3}
+train_corpus=${2}
+n=${3}
 
 
+printf "python_cmd: %s\n" "${python_cmd}"
+printf "n gram: %s\n" "${n}"
+printf "train_corpus: %s\n" "${train_corpus}"
 
-rm -rf multidialect_model_${n}_grams
+rm -rf ${train_corpus}_model_${n}_grams
 
+
+echo "step 1: make index"
 ${python_cmd} langid.py-master/langid/train/index.py ${train_corpus} -m ${train_corpus}_model_${n}_grams
 
+
+echo "step 2: tokenization"
 ${python_cmd} langid.py-master/langid/train/tokenize.py --max_order ${n} ${train_corpus}_model_${n}_grams
 
+
+echo "step 3: feature selection"
 ${python_cmd} langid.py-master/langid/train/DFfeatureselect.py --max_order ${n} ${train_corpus}_model_${n}_grams
 
+echo "step 4: IG weight"
 ${python_cmd} langid.py-master/langid/train/IGweight.py -d ${train_corpus}_model_${n}_grams
 
 ${python_cmd} langid.py-master/langid/train/IGweight.py -lb ${train_corpus}_model_${n}_grams
 
+echo "step 5: LD feature selection"
 ${python_cmd} langid.py-master/langid/train/LDfeatureselect.py ${train_corpus}_model_${n}_grams
 
+
+echo "step 6: train scanner"
 ${python_cmd} langid.py-master/langid/train/scanner.py ${train_corpus}_model_${n}_grams
 
+echo "step 7: NB training"
 ${python_cmd} langid.py-master/langid/train/NBtrain.py ${train_corpus}_model_${n}_grams
 
 #python langid.py -m multidialect_model/model
