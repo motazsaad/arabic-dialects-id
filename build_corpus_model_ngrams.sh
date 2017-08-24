b48186617fed41e45062d08f0df4dcd907269d32
kathrein <kathrein.abu.kwaik@gu.se>
2017-08-24 06:39:25 +0200

    upload padic corpus

--- build_corpus_model_ngrams.sh
+++ build_corpus_model_ngrams.sh
@@ -1,20 +1,21 @@
+#!/usr/bin/env bash
 n=4
-rm -rf Corpus_model_4_grams_${n}_grams
+rm -rf Corpus_model_${n}_grams
 
-python langid.py-master/langid/train/index.py train_our_corpus/ -m Corpus_model_4_grams_${n}_grams
+python langid.py-master/langid/train/index.py Train_Our_Corpus/ -m Corpus_model_${n}_grams
 
-#python langid.py-master/langid/train/tokenize.py --max_order ${n} Corpus_model_4_grams_${n}_grams
+python langid.py-master/langid/train/tokenize.py --max_order ${n} Corpus_model_${n}_grams
 
-#python langid.py-master/langid/train/DFfeatureselect.py --max_order ${n} Corpus_model_4_grams_${n}_grams
+python langid.py-master/langid/train/DFfeatureselect.py --max_order ${n} Corpus_model_${n}_grams
 
-#python langid.py-master/langid/train/IGweight.py -d Corpus_model_4_grams_${n}_grams
+python langid.py-master/langid/train/IGweight.py -d Corpus_model_${n}_grams
 
-#python langid.py-master/langid/train/IGweight.py -lb Corpus_model_4_grams_${n}_grams
+python langid.py-master/langid/train/IGweight.py -lb Corpus_model_${n}_grams
 
-#python langid.py-master/langid/train/LDfeatureselect.py Corpus_model_4_grams_${n}_grams
+python langid.py-master/langid/train/LDfeatureselect.py Corpus_model_${n}_grams
 
-#python langid.py-master/langid/train/scanner.py Corpus_model_4_grams_${n}_grams
+python langid.py-master/langid/train/scanner.py Corpus_model__${n}_grams
 
-#python langid.py-master/langid/train/NBtrain.py Corpus_model_4_grams_${n}_grams
+python langid.py-master/langid/train/NBtrain.py Corpus_model_${n}_grams
 
 #python langid.py -m Corpus_model_4_grams/model
