b8d3df702913fac13fb2395952ccf1e2bcfd8e4a
Motaz Saad <motaz.saad@gmail.com>
2017-08-25 16:29:43 +0300

    rename our corpus and organize sh scripts

--- commands.sh
+++ commands.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
 
+
+# langid training need python 2.7
+
+# uncomment/comment the suitable python version for you
+#py=python2.7
+py=python # some machines python means version 2.7
+
+
+
+# Padic corpus
+bash build_lang_id_model.sh ${py} Train_Padic 4
+bash build_lang_id_model.sh ${py} Train_Padic 5
+
+
+
+
+
+
+
 bash build_lang_id_model.sh python2.7 Train_Padic 5
 bash build_lang_id_model.sh python Train_Padic 5
 
