2c141526e0b413a5d9a45ad0278026d99c1278e1
Motaz Saad <motaz.saad@gmail.com>
2017-08-25 16:32:15 +0300

    cmds to build lang ids for all corpus

--- commands.sh
+++ commands.sh
@@ -15,17 +15,11 @@ bash build_lang_id_model.sh ${py} Train_Padic 4
 bash build_lang_id_model.sh ${py} Train_Padic 5
 
 
+# multidialect_arabic corpus (Nizar corpus)
+bash build_lang_id_model.sh ${py} train_multidialect_arabic 4
+bash build_lang_id_model.sh ${py} train_multidialect_arabic 5
 
 
-
-
-
-bash build_lang_id_model.sh python2.7 Train_Padic 5
-bash build_lang_id_model.sh python Train_Padic 5
-
-
-bash build_lang_id_model.sh python2.7 Train_Our_Corpus 5
-bash build_lang_id_model.sh python Train_Our_Corpus 5
-
-
-bash build_lang_id_model.sh python2.7 train_multidialect_arabic 7
+# our corpus
+bash build_lang_id_model.sh ${py} Train_Our_Corpus 4
+bash build_lang_id_model.sh ${py} Train_Our_Corpus 5
